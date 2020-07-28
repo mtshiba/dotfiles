a016fea99b76f00d971d0f358b7b69019b27ef8c
Shunsuke Shibayama <sbym1346@gmail.com>
2020-07-28 22:14:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,3 +17,12 @@ for f in .??*; do
     [ "$f" = ".git" ] && continue
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# install autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# install color scheme(powerlevel10k)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
