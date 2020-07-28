e8cf367120efe0906b27b5ce798fc35cc9e57fc4
Shunsuke Shibayama <sbym1346@gmail.com>
2020-07-28 20:14:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #...
 
 DOTPATH=~/dotfiles
-# chmod 774 ./lndir.sh
+
+if "$SHELL" = "/bin/bash"; then
+    ZSH_DIR=$(cat /etc/shells | grep zsh)
+    # ZSH not found
+    if [ -z "$ZSH_DIR" ]; then
+        echo "please install and chsh zsh"; exit 1
+    # ZSH found
+    else
+        chsh -s "$ZSH_DIR"
+    fi
+fi
 
 for f in .??*; do
     [ "$f" = ".git" ] && continue
-
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
 done
