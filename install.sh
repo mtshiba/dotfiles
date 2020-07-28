8f0e9695465a5a3538b7e1540734081e60988525
Shunsuke Shibayama <sbym1346@gmail.com>
2020-07-28 22:39:39 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,5 +26,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 # install color scheme(powerlevel10k)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-source ~/.zshrc
