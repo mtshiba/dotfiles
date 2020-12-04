0f0936a6b3fdf9ed7d7e5750f5ab1fbd4763e1b9
Shunsuke Shibayama <sbym1346@gmail.com>
2020-12-04 19:13:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #...
 
-DOTPATH=~/dotfiles
+DOTPATH=${HOME}/dotfiles
 
 if [ "$SHELL" = "/bin/bash" ]; then
     ZSH_DIR=$(cat /etc/shells | grep zsh)
@@ -15,14 +15,14 @@ fi
 
 for f in .??*; do
     [ "$f" = ".git" ] && continue
-    ln -snfv "$DOTPATH/$f" "$HOME/$f"
+    ln -snfv "${DOTPATH}/$f" "${HOME}/$f"
 done
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # install syntax highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 # install autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # install color scheme(powerlevel10k)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
